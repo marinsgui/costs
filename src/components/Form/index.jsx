322fff9802214d1369a8438ff7184405a50aca22
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2022-11-15 10:38:56 -0300

    Conectando com API

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,9 +1,26 @@
+import { useEffect, useState } from 'react';
+
 import Input from '../Input';
 import Select from '../Select';
 import Submit from '../Submit';
 import './styles.css';
 
 export default function Form({ btnText }) {
+
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:5000/categories", {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then((resp) => resp.json())
+            .then((data) => setCategories(data))
+            .catch((err) => console.log(err));
+    }, [])
+
     return (
         <form className='form'>
             <Input 
@@ -20,7 +37,7 @@ export default function Form({ btnText }) {
             placeholder='Insira o orçamento total' 
             />
 
-            <Select name='category-id' text='Selecione a categoria' />
+            <Select name='category-id' text='Selecione a categoria' options={categories} />
 
             <Submit text={btnText} />
         </form>
